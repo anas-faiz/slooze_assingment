2c5e915a61009657f789e873826ce12a136034a5
anas-faiz <anasfaiz0811@gmail.com>
2026-04-28 19:14:06 -0700

    prisma setup completed

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { Prisma, PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+})
+
+const prisma = new PrismaClient(
+    {
+        adapter
+    }
+)
+
+const userData: Prisma.UserCreateInput[]=[
+    {
+        name: "Anas",
+        email: "anasfaiz0811@gmail.com",
+        password: "12345678"
+    }
+]
+
+export async function main(){
+    for (const u of userData){
+        await prisma.user.create({data: u})
+    }
+}
+
+main()
